948f009b437d0553be622b592316aea3d0f540c3
Dmitriy <magifox.dmitriy@gmail.com>
2018-04-20 10:23:45 +0300

    Init Commit

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './components/app.component';
+import { HomeComponent } from './components/home/home.component';
+import { CallbackComponent } from './components/callback/callback.component';
+
+import { AuthService } from './services/auth/auth.service';
+import { ROUTES } from './app.routes';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    CallbackComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(ROUTES)
+  ],
+  providers: [AuthService],
+  bootstrap: [AppComponent]
+})
+
+export class AppModule {}
